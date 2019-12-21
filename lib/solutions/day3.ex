521141b34ae48322503d850ece4590cad55f8df3
ZeLarpMaster <guillaumejean46@gmail.com>
2019-12-20 22:25:55 -0500

    Finishing Day 4

--- lib/solutions/day3.ex
+++ lib/solutions/day3.ex
@@ -10,37 +10,40 @@ defmodule Aoc.Solutions.Day3 do
       |> Enum.map(&parse_wires/1)
 
     %{}
-    |> map_wire(wire1, {0, 0}, :wire1)
-    |> map_wire(wire2, {0, 0}, :wire2)
+    |> map_wire(wire1, {0, 0}, :wire1, 0)
+    |> map_wire(wire2, {0, 0}, :wire2, 0)
     |> find_intersections()
-    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+    |> Enum.map(&elem(&1, 1))
     |> Enum.min()
   end
 
-  defp map_wire(map, [{dx, dy, num} | parts], {x, y}, wire) do
+  defp map_wire(map, [{dx, dy, num} | parts], {x, y}, wire, steps) do
     new_pos = {x + dx, y + dy}
+    steps = steps + 1
 
     map =
-      Map.update(map, new_pos, wire, fn
+      Map.update(map, new_pos, {wire, steps}, fn
         # Can't intersect with yourself
-        val when val == wire -> val
-        _ -> :intersection
+        {^wire, _} = val -> val
+        # Keep shortest number of steps on existing intersections
+        {:intersection, _} = val -> val
+        {_other, other_steps} -> {:intersection, steps + other_steps}
       end)
 
     if num == 1 do
-      map_wire(map, parts, new_pos, wire)
+      map_wire(map, parts, new_pos, wire, steps)
     else
-      map_wire(map, [{dx, dy, num - 1} | parts], new_pos, wire)
+      map_wire(map, [{dx, dy, num - 1} | parts], new_pos, wire, steps)
     end
   end
 
-  defp map_wire(map, [], _, _), do: map
+  defp map_wire(map, [], _, _, _), do: map
 
   defp find_intersections(map) do
     map
     |> Map.to_list()
-    |> Enum.filter(fn {_pos, val} -> val == :intersection end)
-    |> Enum.map(&elem(&1, 0))
+    |> Enum.filter(fn {_pos, {val, _steps}} -> val == :intersection end)
+    |> Enum.map(fn {pos, {:intersection, steps}} -> {pos, steps} end)
   end
 
   defp parse_wires(wires), do: Enum.map(wires, &parse_wire/1)
