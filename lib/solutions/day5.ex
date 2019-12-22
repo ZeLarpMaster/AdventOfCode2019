be2252b913887bcd38076a21218e5490e0506653
ZeLarpMaster <guillaumejean46@gmail.com>
2019-12-21 22:15:09 -0500

    Making progress (but doesn't work)

--- lib/solutions/day5.ex
+++ lib/solutions/day5.ex
@@ -6,6 +6,10 @@ defmodule Aoc.Solutions.Day5 do
     2 => 3,
     3 => 1,
     4 => 1,
+    5 => 2,
+    6 => 2,
+    7 => 3,
+    8 => 3,
     99 => 0
   }
 
@@ -37,22 +41,23 @@ defmodule Aoc.Solutions.Day5 do
     |> case do
       {_memory, :halt} -> :halted
       {memory, offset} -> memory |> run_machine(pc + offset + 1)
+      {_memory, :jump, new_pc} -> memory |> run_machine(new_pc)
     end
   end
 
   # ======== START OF OPCODES ========
   # Addition; arg1 + arg2 -> arg3
-  defp execute_opcode(memory, 1, [{_, left}, {_, right}, {out, _} | _]) do
+  defp execute_opcode(memory, 1, [{_, left}, {_, right}, {out, _}]) do
     {store(memory, out, left + right), 3}
   end
 
   # Multiply; arg1 * arg2 -> arg3
-  defp execute_opcode(memory, 2, [{_, left}, {_, right}, {out, _} | _]) do
+  defp execute_opcode(memory, 2, [{_, left}, {_, right}, {out, _}]) do
     {store(memory, out, left * right), 3}
   end
 
   # Input; stdin -> arg1
-  defp execute_opcode(memory, 3, [{address, _} | _]) do
+  defp execute_opcode(memory, 3, [{address, _}]) do
     value =
       IO.read(:line)
       |> String.trim()
@@ -62,9 +67,29 @@ defmodule Aoc.Solutions.Day5 do
   end
 
   # Output; arg1 -> stdout
-  defp execute_opcode(memory, 4, [{_, value} | _]) do
+  defp execute_opcode(memory, 4, [{_, value}]) do
     IO.puts(value)
     {memory, 1}
+  end
+
+  # If arg1 is non-zero, jump to arg2
+  defp execute_opcode(memory, 5, [{_, 0}, {_dst, _}]), do: {memory, 2}
+  defp execute_opcode(memory, 5, [{_, _}, {dest, _}]), do: {memory, :jump, dest}
+
+  # If arg1 is zero, jump to arg2
+  defp execute_opcode(memory, 6, [{_, 0}, {dest, _}]), do: {memory, :jump, dest}
+  defp execute_opcode(memory, 6, [{_, _}, {_dst, _}]), do: {memory, 2}
+
+  # If arg1 < arg2, 1 -> arg3
+  defp execute_opcode(memory, 7, [{_, left}, {_, right}, {out, _}]) do
+    value = if left < right, do: 1, else: 0
+    {store(memory, out, value), 3}
+  end
+
+  # If arg1 == arg2, 1 -> arg3
+  defp execute_opcode(memory, 8, [{_, left}, {_, right}, {out, _}]) do
+    value = if left == right, do: 1, else: 0
+    {store(memory, out, value), 3}
   end
 
   # Halts the machine
