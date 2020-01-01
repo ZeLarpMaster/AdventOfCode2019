bbea2b538c7fbe278d1c184061707d6cbc92378f
ZeLarpMaster <guillaumejean46@gmail.com>
2020-01-01 09:23:55 -0500

    Completing day 10 part 1

--- lib/solutions/day10.ex
+++ lib/solutions/day10.ex
@@ -10,7 +10,6 @@ defmodule Aoc.Solutions.Day10 do
       |> setify(0)
 
     count_asteroids(asteroid_set, MapSet.to_list(asteroid_set), %{})
-    |> IO.inspect(label: "Count")
     |> Map.to_list()
     |> Enum.max_by(fn {_, count} -> count end)
     |> elem(1)
@@ -19,28 +18,22 @@ defmodule Aoc.Solutions.Day10 do
   defp count_asteroids(_, [], counter), do: counter
 
   defp count_asteroids(set, [pos1 | tail], counter) do
-    count =
-      Enum.count(set, fn pos2 ->
-        IO.inspect(not Enum.any?(set, &in_between?(pos1, &1, pos2)),
-          label: "Can #{inspect(pos1)} see #{inspect(pos2)}"
-        )
-      end)
-
-    count_asteroids(set, tail, Map.put(counter, pos1, count))
+    count = Enum.count(set, fn pos2 -> not Enum.any?(set, &in_between?(pos1, &1, pos2)) end)
+    count_asteroids(set, tail, Map.put(counter, pos1, count - 1))
   end
 
   defp in_between?(p1, middle, p2) when p1 == middle or p2 == middle or p1 == p2, do: false
 
   defp in_between?({x1, y1}, {x2, y2}, {x3, y3}) do
-    dx = x3 - x1
-    dy = y3 - y1
-    gcd = Integer.gcd(abs(dx), abs(dy))
-    dx = div(dx, gcd)
-    dy = div(dy, gcd)
-    dx2 = x2 - x1
-    dy2 = y2 - y1
+    {dx, dy} = normalize_vector(x3 - x1, y3 - y1)
+    {dx2, dy2} = normalize_vector(x2 - x1, y2 - y1)
 
-    div(dx2, dx) == div(dy2, dy) and rem(dx2, dx) == 0 and rem(dy2, dy) == 0
+    dx == dx2 and dy == dy2 and abs(x2 - x1) <= abs(x3 - x1) and abs(y2 - y1) <= abs(y3 - y1)
+  end
+
+  defp normalize_vector(x, y) do
+    gcd = Integer.gcd(abs(x), abs(y))
+    {div(x, gcd), div(y, gcd)}
   end
 
   defp setify([], _), do: MapSet.new()
